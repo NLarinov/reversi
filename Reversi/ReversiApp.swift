cd1964185b2881fa00ea2bf60f4120184e5b23a0
NLarinov <tkachev.n.1506@gmail.com>
2024-11-11 12:12:04 +0300

    Initial Commit

---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by Николай Ткачев on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReversiTests/ReversiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiTests.swift
+//  ReversiTests
+//
+//  Created by Николай Ткачев on 11/11/2024.
+//
+
+import Testing
+@testable import Reversi
+
+struct ReversiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
